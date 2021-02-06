e1edb80fa44e84109c736c3f0f0636bc1fefea1b
Gabriel <gaplogan@gmail.com>
2021-02-06 11:04:37 -0300

    2021.02.06.11.04

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace cs_1._17_ValidaCorreto2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declaração de variáveis
+            double num, raiz;
+
+            //{entrada de dados}
+            do
+            {
+                Console.Write("Digite um número positivo: ");
+                num = double.Parse(Console.ReadLine());
+                
+                Console.WriteLine((num < 0)? "Valor Inválido" : "\n");
+
+            } while (num < 0);
+
+            //{Processamento: Tenta extrair a raiz quadrada do número lido}
+            raiz = Math.Sqrt(num);
+
+            Console.WriteLine($"Raiz quadrada de {num}: {raiz}");
+        }
+    }
+}
